82f4111bd214d5cad7d80efa13dff4749fde2a07
Paul Kim <pkim@hey.com>
2024-05-31 20:38:15 -0400

    Secure password for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+
   validates :email, presence: true, uniqueness: true
   validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
   normalizes :email, with: ->(email) { email.strip.downcase }

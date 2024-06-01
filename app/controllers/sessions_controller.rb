748c018b2c8901aaa391a11d0b7653a713e5dd42
Paul Kim <pkim@hey.com>
2024-05-31 21:29:36 -0400

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate_by(email: params[:email], password: params[:password])
       sign_in(user)
-      redirect_to admin_path
+      redirect_to admin_path, notice: "Welcome Back!"
     else
       flash.now[:alert] = "Invalid email or password"
       render :new, status: :unprocessable_entity

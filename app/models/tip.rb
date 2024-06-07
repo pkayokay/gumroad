10c5d65b988452bb824232c5e3245bc3f5d63b71
Paul Kim <pkim@hey.com>
2024-06-06 21:44:23 -0400

    Add tips to activity

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,4 +1,9 @@
 class Tip < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :target_user, class_name: "User"
+
+
+  def user_display_name
+    user&.display_name || email
+  end
 end
